02c0e5d40fb4c40221a289a6e6b8d5be6cd876fe
Roger Tan <teochewthunder@gmail.com>
2025-11-02 00:26:58 +0800

    Deployment

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module XmasPoll2025
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
