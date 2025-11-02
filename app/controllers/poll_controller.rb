41994bbfbb60a396fa366d54f4922e21bb0f3a73
Roger Tan <teochewthunder@gmail.com>
2025-11-02 13:52:54 +0800

    Styling messaging, clean-up

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -2,9 +2,6 @@ require "httparty"
 require "oauth2"
 
 class PollController < ApplicationController
-  ORDS_TOKEN_URL = ENV["ORDS_TOKEN_URL"]
-  ORDS_CLIENT_ID = ENV["ORDS_CLIENT_ID"]
-  ORDS_CLIENT_SECRET = ENV["ORDS_CLIENT_SECRET"]
   ORDS_API_URL = ENV["ORDS_API_URL"]
 
   def index
@@ -21,8 +18,6 @@ class PollController < ApplicationController
   end
 
   def submit
-
-
     answers = params[:answers]
     payload = { answers: answers }
 
@@ -41,38 +36,5 @@ class PollController < ApplicationController
     end
 
     redirect_to root_path
-  end
-
-  private
-
-  def get_access_token
-    # This example uses a simple token retrieval. For production,
-    # you would cache the token to avoid requesting a new one on every request.
-Rails.logger.info ORDS_CLIENT_ID
-Rails.logger.info ORDS_CLIENT_SECRET
-Rails.logger.info ORDS_TOKEN_URL
-
-    connection_opts = {
-      request: {
-        open_timeout: 15,  # Timeout in seconds for opening the connection
-        read_timeout: 30   # Timeout in seconds for reading the response
-      }
-    }
-  
-    client = OAuth2::Client.new(
-      ORDS_CLIENT_ID,
-      ORDS_CLIENT_SECRET,
-      token_url: ORDS_TOKEN_URL,
-      connection_opts: connection_opts
-    )
-    
-    begin
-      token = client.client_credentials.get_token
-      token.token
-      rescue OAuth2::Error => e
-        Rails.logger.error "OAuth2 Error: #{e.message}"
-        Rails.logger.error e.backtrace.join("\n")
-        nil
-    end
   end
 end
