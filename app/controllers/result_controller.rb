7f17cdf1705482060cd7b68b62b8de3f65c9fbb0
Roger Tan <teochewthunder@gmail.com>
2025-11-07 22:26:14 +0800

    Results controller

---
+++ app/controllers/result_controller.rb
@@ -0,0 +1,18 @@
+require "httparty"
+
+class ResultController < ApplicationController
+  ORDS_API_URL = ENV["ORDS_API_URL"] + "/results"
+
+  def index
+    response = HTTParty.get(
+      ORDS_API_URL
+    )
+
+    if response.code == 200
+      @api_data = response.parsed_response
+    else
+      flash.now[:alert] = "Error fetching data: #{response.body}"
+      @api_data = {}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "poll/index"
+  get "poll/", to: "poll#index"
+  get "result/", to: "result#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
