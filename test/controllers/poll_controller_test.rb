6ff5264614f660893b22c48fc8e79d23e4ad60ea
Roger Tan <teochewthunder@gmail.com>
2025-11-10 14:42:20 +0800

    Poll unit test

--- test/controllers/poll_controller_test.rb
+++ test/controllers/poll_controller_test.rb
@@ -6,4 +6,11 @@ class PollControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_not_nil assigns(:api_data)
   end
+
+  test "should submit results" do
+    post submit_poll_form_url, params: { answers: { "1" => "3", "2" => "5", "3" => "2"} }
+    assert_response :redirect
+    assert_redirected_to root_path
+    assert_not_nil flash[:notice]
+  end
 end
